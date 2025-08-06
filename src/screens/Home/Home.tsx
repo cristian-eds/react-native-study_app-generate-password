a04dca5d0312b6c8b29785eb49ab19376e67cc70
Cristian <cristian.santos0308@gmail.com>
2025-08-06 14:04:40 -0300

    Component BatLogo

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import {  Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
 
-import styles from "./Style";
+import { BatLogo } from '../../components/BatLogo/BatLogo';
+
+import styles from "./HomeStyles";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-        <Text>
-            Home works!!
-        </Text>
-        <StatusBar style="auto" />
+      <View>
+        <BatLogo />
+      </View>
+      <Text>
+        Home works!!
+      </Text>
+      <StatusBar style="auto" />
     </View>
   )
 }
